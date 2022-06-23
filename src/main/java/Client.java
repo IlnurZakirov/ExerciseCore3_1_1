36ccc225761d8fb6fe534d7ce641f2016761d04e
IlnurZakirov <tiro-ne@mail.ru>
2022-06-23 23:55:53 +0500

    first try

---
+++ src/main/java/Client.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) {
+        String host = "127.0.0.1";
+        int port = 8091;
+
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new
+                     PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new
+                     InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("User");
+            String resp = in.readLine();
+            System.out.println(resp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
